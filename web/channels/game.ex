ff2ed107694cc685a95c522574c4cc15fa1656c1
Josh Adams <josh@isotope11.com>
2015-04-15 21:48:44 -0500

    Send basic game state on join

--- web/channels/game.ex
+++ web/channels/game.ex
@@ -2,6 +2,12 @@ defmodule Tetris.GameChannel do
   use Phoenix.Channel
 
   def join("game:play", _auth_msg, socket) do
+    send(self, :after_join)
     {:ok, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    push socket, "tetris:state", %{board: []}
+    {:noreply, socket}
   end
 end
