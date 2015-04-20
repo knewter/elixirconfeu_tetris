deaa10ed4161ab1b3320a11b8baacc2b38faec96
Josh Adams <josh.rubyist@gmail.com>
2015-04-20 12:36:48 -0500

    Game becomes GenServer

--- lib/tetris/game.ex
+++ lib/tetris/game.ex
@@ -1,8 +1,21 @@
 defmodule Tetris.Game do
+  use GenServer
+
   defmodule State do
     defstruct [:board, :next]
   end
+
+  ## Public API
   def start do
+    {:ok, pid} = GenServer.start(__MODULE__, [])
+  end
+
+  def get_state(pid) do
+    GenServer.call(pid, :get_state)
+  end
+
+  ## Server callbacks
+  def init(_args) do
     {:ok, %Tetris.Game.State{
              board: [
                [0,0,0,0,1,0,0,0,0,0],
@@ -34,10 +47,11 @@ defmodule Tetris.Game do
     }
   end
 
-  def get_state(game) do
-    %{
-      board: game.board,
-      next: game.next
+  def handle_call(:get_state, _from, state) do
+    reply_state = %{
+      board: state.board,
+      next: state.next
     }
+    {:reply, reply_state, state}
   end
 end
